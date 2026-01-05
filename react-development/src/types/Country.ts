b57fbb96af709d792e414f1adc99c8dcfc74bf76
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-05 12:25:17 -0500

    created the country.ts file interface.

---
+++ react-development/src/types/Country.ts
@@ -0,0 +1,31 @@
+export interface Country {
+  name: {
+    common: string;
+    official: string;
+    nativeName?: {
+        [key: string]: {
+            official: string;
+            common: string;
+        };
+    };
+}
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  languages: {
+    [key: string]: string;
+  };
+  capital: string[];
+  region: string;
+  subregion?: string;    
+  population: number;
+  flags: {
+    png: string;
+    svg: string;
+  };
+  tld?: string[];        
+  borders?: string[];  
+}; 
