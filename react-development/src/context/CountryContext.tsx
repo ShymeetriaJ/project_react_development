a454d8e1f35359053805e6704cc4a168134471ff
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-05 16:56:20 -0500

    added props and the CountryContext wrapper to the return.

--- react-development/src/context/CountryContext.tsx
+++ react-development/src/context/CountryContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, ReactNode } from "react-router-dom";
-import { Country } from "../types/Country";
+import type { Country } from "../types/Country";
+
 
 
 interface CountryContextType {
@@ -8,4 +9,29 @@ interface CountryContextType {
     error: string | null;
 }
 const CountryContext = createContext<CountryContextType | undefined>(undefined);
+
+export const CountryProvider = ({Children}: CountryProviderProps) => {
+    const {data, loading} = useFetch(url);
+
+    const value = {
+        countries: data || [],
+        loading: loading,
+    };
+
+    return(
+        <CountryContext.Provider value={value}>
+            {Children}
+        </CountryContext.Provider>
+    );
+}; 
+export const useCountryContext = () => {
+    const context = useContext(CountryContext);
+  
+    
+    if (context === undefined) {
+        throw new Error('useCountryContext must be used within CountryProvider');
+    }
+    
+    return context;              
+};
  
