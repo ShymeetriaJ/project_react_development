263691ab8c497c5adc3f78b6d2844bbf7ebd52e7
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-06 11:57:12 -0500

    Repurposed previously used  spinner logic; spinner logic added to spinner.tsx file.

---
+++ react-development/src/components/Spinner.tsx
@@ -0,0 +1,10 @@
+const Spinner = () => {
+
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+      <p className="spinner-text">Loading...</p>
+    </div>
+  );
+};
+export default Spinner;
