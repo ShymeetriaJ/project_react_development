ce63ba4c95c979e49f831f9625c5cb0b4c47dcdd
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-06 16:11:03 -0500

    updated countrycard.tsx file.

--- react-development/src/pages/Home.tsx
+++ react-development/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import { useCountryContext } from '../context/CountryContext';
 import { Navbar } from '../components/Navbar';
-import { SearchBar } from '../components/SearchBar';
 import { FilterDropdown } from '../components/FilterDropdown';
 import { CountryCard } from '../components/CountryCard';
 import { Spinner } from '../components/Spinner';
 import { ErrorMessage } from '../components/ErrorMessage';
+import { SearchBar } from '../components/Searchbar';
 
 
 export const Home = () => {
