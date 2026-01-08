44e42b3a77fe78131a51693b45438eabc45457b3
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-08 16:54:47 -0500

    changed package.json for git deployment.

--- react-development/src/App.tsx
+++ react-development/src/App.tsx
@@ -1,18 +1,18 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { CountryProvider } from './context/CountryContext';
 import { Home } from './pages/Home';
 import { CountryDetail } from './pages/CountryDetail';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <CountryProvider>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/country/:code" element={<CountryDetail />} />
           </Routes>
         </CountryProvider>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
